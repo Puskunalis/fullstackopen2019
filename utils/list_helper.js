822c596e6c9b382aaf3634e145aab9b4d3f3d4b8
Vilius Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2019-11-04 01:51:51 -0800

    Complete exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,21 @@
-const dummy = (blogs) => {
-  return 1
-}
+const dummy = (blogs) => 1
 
-const totalLikes = (blogs) => {
-  return blogs.reduce((likes, blog) => likes + blog.likes, 0)
+const totalLikes = (blogs) => blogs.reduce((likes, blog) => likes + blog.likes, 0)
+
+const favoriteBlog = (blogs) => {
+  const max = blogs.reduce((a, b) => {
+    return (a.likes > b.likes) ? a : b
+  })
+
+  delete max._id
+  delete max.url
+  delete max.__v
+  
+  return max
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
