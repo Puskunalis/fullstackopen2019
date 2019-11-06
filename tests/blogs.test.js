e21d45948548572821f08564eecd2d6602526fd3
Vilius Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2019-11-06 02:35:14 -0800

    Complete exercise 4.9

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -5,15 +5,29 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  const res = await api
+let res
+
+beforeEach(async () => {
+  res = await api
     .get('/api/blogs')
+})
+
+test('blogs are returned as json', async () => {
+  res
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
   const length = await Blog.countDocuments({})
 
   expect(res.body.length).toBe(length)
+})
+
+test('blogs have id property', async () => {
+  res
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  
+  expect(res.body[0].id).toBeDefined()
 })
 
 afterAll(() => {
