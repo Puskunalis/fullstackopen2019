91a02b00c7f5bf36f0f09fc32ace8cc7ef46b757
Vilius Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2019-11-06 02:58:47 -0800

    Complete exercise 4.10

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -5,15 +5,56 @@ const app = require('../app')
 
 const api = supertest(app)
 
-let res
+const blogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2
+  }
+]
 
 beforeEach(async () => {
-  res = await api
-    .get('/api/blogs')
+  await Blog.deleteMany({})
+
+  const promiseArray = blogs.map(blog => new Blog(blog).save())
+
+  await Promise.all(promiseArray)
 })
 
 test('blogs are returned as json', async () => {
-  res
+  const res = await api
+    .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
@@ -23,11 +64,36 @@ test('blogs are returned as json', async () => {
 })
 
 test('blogs have id property', async () => {
-  res
+  const res = await api
+    .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-  
+
   expect(res.body[0].id).toBeDefined()
+})
+
+test('new blogs are added', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful blah blah blah',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  }
+
+  const length = await Blog.countDocuments({})
+
+  const res = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  let blogsAtEnd = await Blog.find({})
+  blogsAtEnd = blogsAtEnd.map(blog => blog.toJSON())
+
+  expect(blogsAtEnd.length).toBe(length + 1)
+
+  expect(blogsAtEnd.map(blog => blog.title)).toContain('Go To Statement Considered Harmful blah blah blah')
 })
 
 afterAll(() => {
