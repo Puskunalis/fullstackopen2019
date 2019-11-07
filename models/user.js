ab3dfd642a5ed010378eff9e6a89e85294c2172e
Vilius Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2019-11-07 06:11:48 -0800

    Complete exercise 4.15

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  passwordHash: String
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.passwordHash
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
