d8e3bdd39ef790716ab8cd2ef484ee6baa842bd2
Vilius Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2019-11-08 04:41:52 -0800

    Complete exercise 4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -42,8 +42,18 @@ blogsRouter.post('/', async (req, res, next) => {
 
 blogsRouter.delete('/:id', async (req, res) => {
   try {
-    await Blog.findByIdAndRemove(req.params.id)
-    res.status(204).end()
+    const decodedToken = jwt.verify(req.token, process.env.SECRET)
+    if (!req.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(req.params.id)
+
+    if (blog.user.toString() === user.id.toString()) {
+      await Blog.findByIdAndRemove(req.params.id)
+      res.status(204).end()
+    }
   } catch {
     res.status(400).json({ 'error': 'blog not found' })
   }
