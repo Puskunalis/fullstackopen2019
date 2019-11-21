f7f64eceb2e84a68931ded829bf7e26568f29c70
Vilius Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2019-11-21 07:14:22 -0800

    Complete exercise 6.7 & 6.8

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,38 +1,14 @@
 import React from 'react'
-import { createAnecdote, addVote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = props => {
-  const anecdotes = props.store.getState()
-
-  const vote = id => {
-    props.store.dispatch(addVote(id))
-  }
-
-  const addAnecdote = event => {
-    event.preventDefault()
-    props.store.dispatch(createAnecdote(event.target.anecdote.value))
-    event.target.anecdote.value = ''
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm store={props.store} />
+      <AnecdoteList store={props.store} />
     </div>
   )
 }
