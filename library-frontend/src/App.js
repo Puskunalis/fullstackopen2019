3a5e7e10b72888f238c0cf96d534da8e64c29b70
Vilius Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2019-12-13 04:49:18 -0800

    Complete exercise 8.10

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 
 const ALL_AUTHORS = gql`
@@ -25,14 +25,50 @@ const ALL_BOOKS = gql`
   }
 `
 
+const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      author: $author,
+      published: $published,
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
+
 const App = () => {
   const [page, setPage] = useState('authors')
 
   const authors = useQuery(ALL_AUTHORS)
   const books = useQuery(ALL_BOOKS)
 
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const handleError = (error) => {
+    setErrorMessage(error.graphQLErrors[0].message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  const [addBook] = useMutation(ADD_BOOK, {
+    onError: handleError,
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }]
+  })
+
   return (
     <div>
+      {errorMessage &&
+        <div style={{ color: 'red' }}>
+          {errorMessage}
+        </div>
+      }
+
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
@@ -43,9 +79,7 @@ const App = () => {
 
       <Books show={page === 'books'} result={books} />
 
-      <NewBook
-        show={page === 'add'}
-      />
+      <NewBook show={page === 'add'} addBook={addBook} />
 
     </div>
   )
--- library-frontend/src/components/NewBook.js
+++ library-frontend/src/components/NewBook.js
@@ -14,7 +14,9 @@ const NewBook = (props) => {
   const submit = async (e) => {
     e.preventDefault()
 
-    console.log('add book...')
+    await props.addBook({
+      variables: { title, published: +published, author, genres }
+    })
 
     setTitle('')
     setPublished('')
